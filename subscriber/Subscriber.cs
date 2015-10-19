d73d8fda733ea5f65f69b9ab4d1296b19bea0fcd
jparvela <jparvela@gmail.com>
2015-10-19 21:36:02 +0100

    Update interface of subscriber project.

--- subscriber/Subscriber.cs
+++ subscriber/Subscriber.cs
@@ -1,19 +1,12 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Subscriber {
-    class Subscriber {
-        static void Main(string[] args) {
-            //TODO: Something
-        }
-
-        private String name;
-        private String processURL;
-        private ArrayList subscriptions;
+    class Subscriber : MarshalByRefObject {
+        private string name;
+        private string processURL;
+        private List<string> subscriptions;
+        //TODO: create type for subscriptions
 
         public Subscriber() {
             //TODO: something
@@ -24,6 +17,11 @@ namespace Subscriber {
         }
 
         public void unSubscribe(String site) {
+            //TODO: something
+        }
+
+        // Callback for brokers to use to send the data
+        public void newPublication(string topic, string content) {
             //TODO: something
         }
     }
