fcd2b0130969ada6597f36062a7480367a35e036
Gonçalo Rodrigues <goncalo.rodrigues@rnl.tecnico.ulisboa.pt>
2015-10-27 16:07:36 +0000

    Created argument handling in Broker's Main method

--- Broker/BrokerProgram.cs
+++ Broker/BrokerProgram.cs
@@ -7,7 +7,39 @@ using System.Threading.Tasks;
 namespace Broker {
     class BrokerProgram {
         static void Main(string[] args) {
-            //TODO: something
+            //TODO: validade input
+
+            //processName processURL site routingtype puppetMasterURL -p parentURL -c childURL
+            string processName = args[0];
+            string processURL = args[1];
+            string site = args[2];
+            string routing = args[3];
+            string puppetMasterURL = args[4];
+
+            List<string> childList = new List<string>();
+            List<string> parentList = new List<string>();
+
+            for (int i = 5; i < args.Length; i+=2) {
+                switch (args[i]) {
+                    case "-p":
+                        parentList.Add(args[i + 1]);
+                        break;
+                    case "-c":
+                        childList.Add(args[i + 1]);
+                        break;
+                }
+            }
+
+            //initialization of data finished
+            Broker b = new Broker(processName, processURL, site, routing, puppetMasterURL);
+            foreach (string c in childList) {
+                b.addChildUrl(c);
+            }
+            foreach (string p in parentList) {
+                b.addParentUrl(p);
+            }
+            while (true) ;
+            //TODO: actual code with broker b
         }
     }
 }
